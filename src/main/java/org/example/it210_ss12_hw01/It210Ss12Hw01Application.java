6cb34bb58a6ec4b2c759c4e08392394a06dd1b18
KingDragon PC <nglinh150606@gmail.com>
2026-04-21 21:27:43 +0700

    first commit

---
+++ src/main/java/org/example/it210_ss12_hw01/It210Ss12Hw01Application.java
@@ -0,0 +1,13 @@
+package org.example.it210_ss12_hw01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It210Ss12Hw01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It210Ss12Hw01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/it210_ss12_hw01/It210Ss12Hw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.it210_ss12_hw01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It210Ss12Hw01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
